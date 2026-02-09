ed57ed986996d4ba58721d5d4e885fad8c819d63
Artgod13 <bainarthur4@gmail.com>
2026-02-08 20:01:27 -0600

    Initial commit

---
+++ src/main/java/net/artgod13/extrafarmingaddonmod/ExtraFarmingAddOnModClient.java
@@ -0,0 +1,10 @@
+package net.artgod13.extrafarmingaddonmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtraFarmingAddOnModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/artgod13/extrafarmingaddonmod/ExtraFarmingAddOnModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.artgod13.extrafarmingaddonmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ExtraFarmingAddOnModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
